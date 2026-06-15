c6310b47bcb29b086c6ec2c265cfd5f5c874c1ac
THIAGO ANTONIO BAIERSKI <thiago.baierski@univille.br>
2026-06-15 20:35:19 -0300

    Database atualizada

--- database/atende_lab.sql
+++ database/atende_lab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Jun 16, 2026 at 12:25 AM
+-- Generation Time: Jun 16, 2026 at 01:34 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `atendimentos` (
   `id` int(11) NOT NULL,
-  `id_tipos_atendimento` int(11) NOT NULL,
+  `id_tipo_atendimento` int(11) NOT NULL,
   `id_pessoa` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `data_atendimento` date NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE `atendimentos` (
 -- Dumping data for table `atendimentos`
 --
 
-INSERT INTO `atendimentos` (`id`, `id_tipos_atendimento`, `id_pessoa`, `id_usuario`, `data_atendimento`, `hora_atendimento`, `descricao`, `observacao_final`, `status`, `criado_em`) VALUES
+INSERT INTO `atendimentos` (`id`, `id_tipo_atendimento`, `id_pessoa`, `id_usuario`, `data_atendimento`, `hora_atendimento`, `descricao`, `observacao_final`, `status`, `criado_em`) VALUES
 (1, 1, 1, 1, '2026-06-09', '20:58:00', 'Solicitação de boletim escolar', 'Solicitação completa e documento enviado', 'inativo', '2026-06-10 22:47:33');
 
 -- --------------------------------------------------------
@@ -62,15 +62,17 @@ CREATE TABLE `pessoas` (
   `curso` varchar(100) NOT NULL,
   `periodo` varchar(100) NOT NULL,
   `status` varchar(100) NOT NULL,
-  `criado_em` timestamp NOT NULL DEFAULT current_timestamp()
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  `observacoes` text DEFAULT NULL,
+  `atualizado_em` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pessoas`
 --
 
-INSERT INTO `pessoas` (`id`, `nome`, `documento`, `email`, `telefone`, `curso`, `periodo`, `status`, `criado_em`) VALUES
-(1, 'Matheus', 'documento', 'matheus@gmail.com', '+55 (47)11223-4456', 'engenharia de software', '5º Semestre', 'ativo', '2026-06-10 22:47:33');
+INSERT INTO `pessoas` (`id`, `nome`, `documento`, `email`, `telefone`, `curso`, `periodo`, `status`, `criado_em`, `observacoes`, `atualizado_em`) VALUES
+(1, 'Matheus', 'documento', 'matheus@gmail.com', '+55 (47)11223-4456', 'engenharia de software', '5º Semestre', 'ativo', '2026-06-10 22:47:33', NULL, '2026-06-15 22:37:09');
 
 -- --------------------------------------------------------
 
@@ -83,15 +85,16 @@ CREATE TABLE `tipos_atendimento` (
   `nome` varchar(100) NOT NULL,
   `descricao` text NOT NULL,
   `status` enum('ativo','inativo') DEFAULT 'ativo',
-  `criado_em` timestamp NOT NULL DEFAULT current_timestamp()
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  `atualizado_em` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tipos_atendimento`
 --
 
-INSERT INTO `tipos_atendimento` (`id`, `nome`, `descricao`, `status`, `criado_em`) VALUES
-(1, 'Boletim', 'Solicitação de boletim escolar', 'ativo', '2026-06-10 22:47:33');
+INSERT INTO `tipos_atendimento` (`id`, `nome`, `descricao`, `status`, `criado_em`, `atualizado_em`) VALUES
+(1, 'Boletim', 'Solicitação de boletim escolar', 'ativo', '2026-06-10 22:47:33', '2026-06-15 22:37:09');
 
 -- --------------------------------------------------------
 
@@ -106,16 +109,17 @@ CREATE TABLE `usuarios` (
   `senha` varchar(255) NOT NULL,
   `perfil` enum('admin','atendente','aluno') DEFAULT 'atendente',
   `status` enum('ativo','inativo') DEFAULT 'ativo',
-  `criado_em` timestamp NOT NULL DEFAULT current_timestamp()
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  `atualizado_em` timestamp NOT NULL DEFAULT curtime() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `perfil`, `status`, `criado_em`) VALUES
-(1, 'Administrador', 'admin@atendelab.com', '$2y$10$J9P2kU2BAMZ3TZcuxTsW4e1D/lka8EocYHzvyoOZmCNcWDQz3RuVC', 'admin', 'ativo', '2026-06-10 22:47:33'),
-(3, 'Jorge Atualizado', 'jorge@atendente.com', '$2y$10$q7TDgyzrjr55l1fHgJnYiuR7748mmT46NI30i4I10fVn4j/K2CPEq', 'atendente', 'ativo', '2026-06-10 22:56:20');
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `perfil`, `status`, `criado_em`, `atualizado_em`) VALUES
+(1, 'Administrador', 'admin@atendelab.com', '$2y$10$J9P2kU2BAMZ3TZcuxTsW4e1D/lka8EocYHzvyoOZmCNcWDQz3RuVC', 'admin', 'ativo', '2026-06-10 22:47:33', '2026-06-15 22:37:09'),
+(3, 'Jorge Atualizado', 'jorge@atendente.com', '$2y$10$q7TDgyzrjr55l1fHgJnYiuR7748mmT46NI30i4I10fVn4j/K2CPEq', 'atendente', 'ativo', '2026-06-10 22:56:20', '2026-06-15 22:37:09');
 
 --
 -- Indexes for dumped tables
@@ -126,7 +130,7 @@ INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `perfil`, `status`, `cri
 --
 ALTER TABLE `atendimentos`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_atendimentos_tipos_atendimento` (`id_tipos_atendimento`),
+  ADD KEY `fk_atendimentos_tipo_atendimento` (`id_tipo_atendimento`),
   ADD KEY `fk_atendimentos_pessoas` (`id_pessoa`),
   ADD KEY `fk_atendimentos_usuarios` (`id_usuario`);
 
@@ -187,7 +191,7 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `atendimentos`
   ADD CONSTRAINT `fk_atendimentos_pessoas` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoas` (`id`),
-  ADD CONSTRAINT `fk_atendimentos_tipos_atendimento` FOREIGN KEY (`id_tipos_atendimento`) REFERENCES `tipos_atendimento` (`id`),
+  ADD CONSTRAINT `fk_atendimentos_tipo_atendimento` FOREIGN KEY (`id_tipo_atendimento`) REFERENCES `tipos_atendimento` (`id`),
   ADD CONSTRAINT `fk_atendimentos_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
